8de1a7e2c7cc74afe775df3f4691e891d9289085
Shubham0214 <shubhedu0112@gmail.com>
2024-12-15 15:51:57 +0530

    mid functions

---
+++ JavaScript/Part4/functions.js
@@ -0,0 +1,28 @@
+/*
+1. Write a function named 'maketea' that takes one parameter , 'typeOfTea' and returns a string like "Making green tea" when called with "Green tea" 
+Store the result in a variable named 'teaOrder'
+ */
+
+// function greet(name) {
+//   console.log('Hello ${name}');
+// }
+
+// greet("Shubh");
+
+function maketea(typeOfTea) {
+  return "Making &{typeOfTea}";
+  console.log(test);
+}
+
+let teaOrder = maketea("Lemon tea");
+console.log(teaOrder);
+
+/*
+ 2. Create a function named 'orderTea' that takes one parameter , 'teatype' . Inside this function, create another function named confirmOrder that returns a message like "Order confirmed for chai"
+
+ call 'confirmedOrder' from within 'orderTea' and return the result.
+ */
+
+function orderTea(teatype) {
+  function confirmOrder() {}
+}
